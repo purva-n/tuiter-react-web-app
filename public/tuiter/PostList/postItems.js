f48fcc9b2746586c0b0bb77a0526778291dfe845
Purva Nartam <purva.8998@gmail.com>
2023-02-19 21:43:17 -0500

    A5 Javascript submission Sp23

---
+++ public/tuiter/PostList/postItems.js
@@ -0,0 +1,30 @@
+const postItems = [
+    {
+        topic: 'Amazing show about @inspiration4x mission',
+        userName: 'Elon Musk',
+        handle: 'elonmusk',
+        caption: 'From training to launch to landing, this is an all-access docuseries rides along with Inspiration4 crew on the first all-civilian orbital space',
+        caption_title: 'Countdown: Inspiration Mission to Space | Netflix Official Site',
+        time: '23h',
+        image: '../../images/inspiration.jpeg',
+        comment: '4.2K',
+        retweets: '3.5K',
+        likes: '37.5K',
+        profileImg: '../../images/elon.png',
+    },
+    {
+        topic: 'Artificial Intelligence is so ingained in our daily lives, even if we don\'t realize it. What exactly is it, though?',
+        userName: 'WebMD',
+        handle: 'WebMD',
+        caption: '',
+        time: 'Jun 22',
+        title: '',
+        image: '../../images/webmd.jpg',
+        comment: '4K',
+        retweets: '22K',
+        likes: '12K',
+        profileImg: '../../images/webmdlogo.svg'
+    },
+];
+
+export default postItems;
